d2ab68374c7034a1b4217b6acf9c7fe6be055370
Dillon Huff <dillonhuff@gmail.com>
2015-06-30 17:11:12 +0200

    InterchangeAndFuse is now more comprehensive

--- src/InterchangeAndFuse.hs
+++ src/InterchangeAndFuse.hs
@@ -1,13 +1,29 @@
 module InterchangeAndFuse(interchangeAndFuse) where
 
+import Data.List as L
+
 import Fusion
 import Statement
 import StatementInterchange
 
 interchangeAndFuse stmts = applyToLoopBodiesBU (tryToInterchangeAndFuse $ iVarRanges stmts) stmts
 
-tryToInterchangeAndFuse iRanges (l1:r:l2:rest) =
-  case canFuseIfAdjacent l1 l2 && canInterchange iRanges r l2 of
-    True -> (fuseLoops l1 l2):(tryToInterchangeAndFuse iRanges (r:rest))
-    False -> l1 : (tryToInterchangeAndFuse iRanges (r:l2:rest))
+tryToInterchangeAndFuse iRanges (l1:stmts) =
+  case nextLoopAndStmtsBetween stmts of
+    Just (l2, stmtsBetween, rest) -> 
+      case canFuseIfAdjacent l1 l2 && (L.and $ L.map (\stmt -> canInterchange iRanges stmt l2) stmtsBetween) of
+        True -> tryToInterchangeAndFuse iRanges ((fuseLoops l1 l2):(stmtsBetween ++ rest))
+        False -> l1 : (tryToInterchangeAndFuse iRanges (stmtsBetween ++ [l2] ++ rest))
+    Nothing -> l1:stmts
 tryToInterchangeAndFuse iRanges other = other
+
+nextLoopAndStmtsBetween [] = Nothing
+nextLoopAndStmtsBetween stmts =
+  let stmtsBetween = L.takeWhile (\stmt -> not $ isLoop stmt) stmts in
+  case L.length stmtsBetween == L.length stmts of
+    True -> Nothing
+    False ->
+      let loopAndStmtsAfter = L.dropWhile (\stmt -> not $ isLoop stmt) stmts
+          nextLoop = L.head loopAndStmtsAfter
+          stmtsAfter = L.tail loopAndStmtsAfter in
+      Just (nextLoop, stmtsBetween, stmtsAfter)
