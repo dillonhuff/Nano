c00be0fb2ea3379ad6d936697466ead61f5bb2f5
Dillon Huff <dillonhuff@gmail.com>
2015-07-01 23:04:07 +0200

    Added more tests of temp compaction

--- test/SystemTests/CompactTemps.hs
+++ test/SystemTests/CompactTemps.hs
@@ -13,10 +13,19 @@ import Scalarization
 import Statement
 
 allCompactTempsTests = TestLabel "All scalarization system tests" $
-                      TestList $ L.map (\op -> TestCase $ assertOptimizationsCorrect compactTempsOpts op) compoundTestOperations
+                      TestList $ compactTests
+
+compactTests =
+  [TestCase $ assertOptimizationsCorrect compactTempsOpts [matrixAdd a b c],
+   TestCase $ assertOptimizationsCorrect compactTempsOpts [matrixAdd tr9c9 b b, matrixAdd a tr9c9 tr9c9],
+   TestCase $ assertOptimizationsCorrect compactTempsOpts [matrixAdd tr9c9 a a, matrixAdd a b tr9c9],
+   TestCase $ assertOptimizationsCorrect compactTempsOpts [scalarMultiply tr9c9 alpha a, matrixAdd a tr9c9 tr9c9],
+   TestCase $ assertOptimizationsCorrect compactMMulOpts [scalarMultiply tr9c9 alpha a, matrixMultiply c tr9c9 b],
+   TestCase $ assertOptimizationsCorrect compactTransOpts [matrixTranspose tr9c9 a, matrixAdd c b tr9c9]]
 
 compactTempsOpts = (scalarize "r_"):compactTemps:preprocessingOpts
---compactTempsOpts = (scalarize "r_"):preprocessingOpts
+compactMMulOpts = (scalarize "r_"):compactTemps:preprocessMMulOpts
+compactTransOpts = (scalarize "r_"):compactTemps:fuseInnerLoops:preprocessTransOpts
 
 preprocessingOpts =
   L.intersperse fuseInnerLoops $ 
@@ -35,5 +44,22 @@ preprocessingOpts =
    blockMatrixTransposeM (iVar "i8") (iConst 1)]
 
 
-
+preprocessMMulOpts =
+  L.intersperse fuseInnerLoops $
+  L.map (\t -> expandStatementsBU t)
+  [blockMatrixMultiplyN (iVar "i7") (iConst 1),
    
+   blockMatrixMultiplyP (iVar "i4") (iConst 1),
+   blockScalarMultiplyN (iVar "i3") (iConst 1),
+   
+   blockMatrixMultiplyM (iVar "i2") (iConst 1),
+   blockScalarMultiplyM (iVar "i1") (iConst 1)]
+
+preprocessTransOpts =
+  L.intersperse fuseInnerLoops $
+  L.map (\t -> expandStatementsBU t)
+  [blockMatrixTransposeN (iVar "i1") (iConst 1),
+   blockMatrixAddN (iVar "i2") (iConst 1),
+   
+   blockMatrixTransposeM (iVar "i3") (iConst 1),
+   blockMatrixAddM (iVar "i4") (iConst 1)]
