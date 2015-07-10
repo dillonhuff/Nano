06ff8be0991241e254d948435770ad6de1b54b54
Dillon Huff <dillonhuff@gmail.com>
2015-07-10 19:22:48 +0200

    AVX optimizations need to be adjusted, but overall the changes are going well.

--- test/SystemTests/BasicGS.hs
+++ test/SystemTests/BasicGS.hs
@@ -4,20 +4,24 @@ import Data.List as L
 import Test.HUnit
 
 import Blocking
+import CBackEnd.CodeGeneration.AVX.Double
 import CBackEnd.CodeGeneration.Common
 import CBackEnd.CodeGeneration.Function
 import CBackEnd.CodeGeneration.Scalar
-import Dummies
+import Dummies hiding (daxpy)
 import Fuzz
 import IndexExpression
 import Matrix
+import OptimizationGroups.AVXLevel1
 import Statement
 
 allBasicGSTests =
   TestLabel "All basic general size tests" $
   TestList $ L.map TestCase
-  [assertOptimizationsCorrectGS scalarVarDecls toCStmtsFunction [] (dscal (iVar "n")),
-   assertOptimizationsCorrectGS scalarVarDecls toScalarC blockingT (dscal (iVar "m"))]
+  [assertOptimizationsCorrectGS scalarVarDecls toCStmtsFunction [] (dscal (iVar "m")),
+   assertOptimizationsCorrectGS scalarVarDecls toScalarC blockingT (dscal (iVar "m")),
+   assertOptimizationsCorrectGS avxVarDeclsDouble toAVXDouble (avxLvl1Opts 4) (dscal (iVar "m")),
+   assertOptimizationsCorrectGS avxVarDeclsDouble toAVXDouble (avxLvl1Opts 4) (daxpy (iVar "m"))]
 
 blockingT =
   L.map (\t -> expandStatementsBU t)
@@ -25,5 +29,12 @@ blockingT =
 
 dscal i =
   let alpha = constDblMat "alpha" 1 1 1 1
-      x = matrix "x" (iVar "n") (iConst 1) (iConst 1) (iConst 1) (properties arg double memory) in
+      x = matrix "x" i (iConst 1) (iConst 1) (iConst 1) (properties arg double memory) in
   [scalarMultiply x alpha x]
+
+daxpy i =
+  let alpha = constDblMat "alpha" 1 1 1 1
+      y = matrix "y" i (iConst 1) (iConst 1) (iConst 1) (properties arg double memory)
+      t = matrix "t" i (iConst 1) (iConst 1) (iConst 1) (properties local double memory)
+      x = matrix "x" i (iConst 1) (iConst 1) (iConst 1) (properties arg double memory) in
+  [scalarMultiply t alpha x, matrixAdd y t y]
