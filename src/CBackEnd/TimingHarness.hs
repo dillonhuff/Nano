4311f62a5f1676a3e3926fcce461a9e14c96e87d
Dillon Huff <dillonhuff@gmail.com>
2015-07-10 11:03:25 +0200

    The setup code and function call generation for timing are now separated from the generation of the timing code boilerplate.

--- src/CBackEnd/TimingHarness.hs
+++ src/CBackEnd/TimingHarness.hs
@@ -7,10 +7,19 @@ import CBackEnd.Utils
 
 timingHarness :: String -> [BufferInfo] -> CTopLevelItem String
 timingHarness funcName bufInfo =
+  timingHarnessS ft bufDecs allocAndSetRand freeBufs
+  where
+    ft = cExprSt (cFuncall funcName $ L.map (\info -> cVar $ bufName info) bufInfo) ""
+    allocAndSetRand = (L.map initializeBuffer bufInfo) ++ (L.map setArgToRandValuesCode bufInfo)
+    freeBufs = L.map freeBuffer bufInfo
+    bufDecs = bufDecls bufInfo
+
+timingHarnessS :: CStmt String -> [(CType, String)] -> [CStmt String] -> [CStmt String] -> CTopLevelItem String
+timingHarnessS funcallToTime varDecls setupCode tearDownCode =
   cFuncDecl cVoid "time_impl" [(cPtr cFILE, "df")] $ cBlock allDecls bodyStmts
   where
-    allDecls = (bufDecls bufInfo) ++ timingVarDecls
-    bodyStmts = timingBody funcName bufInfo
+    allDecls = varDecls ++ timingVarDecls
+    bodyStmts = timingBody funcallToTime setupCode tearDownCode
 
 timingVarDecls = [(cULongLong, "start"),
                   (cULongLong, "end"),
@@ -19,45 +28,37 @@ timingVarDecls = [(cULongLong, "start"),
                   (cULongLong, "num_runs"),
                   (cDouble, "avg_cycles_per_run")]
 
-timingBody funcName bufInfo =
-  bufferAllocation ++
-  setBuffersToRandomValues ++
-  (runCountLoop funcName bufInfo) ++
-  (timedLoop funcName bufInfo) ++
+timingBody funcallToTime setupCode tearDownCode =
+  setupCode ++
+  (runCountLoop funcallToTime) ++
+  (timedLoop funcallToTime) ++
   avgCyclesPerRunComputation ++
   writeTimingResults ++
-  bufferFreeing
-  where
-    bufferAllocation = L.map initializeBuffer bufInfo
-    setBuffersToRandomValues = L.map setArgToRandValuesCode bufInfo
-    bufferFreeing = L.map freeBuffer bufInfo
-
-runCountLoop funcName bufInfo =
+  tearDownCode
+  
+runCountLoop funcallToTime =
   [cExprSt (cAssign (cVar "num_runs") (cIntLit 0)) "",
    cExprSt (cAssign (cVar "total_cycles") (cIntLit 0)) ""] ++
-  (runCountBody funcName bufInfo)
+  (runCountBody funcallToTime)
 
-runCountBody funcName bufInfo =
-  [cWhile (cLEQ (cVar "total_cycles") (cIntLit 100000000)) (runCountWhileBody funcName bufInfo) ""]
+runCountBody funcallToTime =
+  [cWhile (cLEQ (cVar "total_cycles") (cIntLit 100000000)) (runCountWhileBody funcallToTime) ""]
 
-runCountWhileBody funcName bufInfo =
+runCountWhileBody funcallToTime =
   cBlock [] body
   where
     addToTotal = cExprSt (cAssign (cVar "total_cycles") (cAdd (cVar "total_cycles") (cSub (cVar "start") (cVar "end")))) ""
     incRuns = cExprSt (cAssign (cVar "num_runs") (cAdd (cVar "num_runs") (cIntLit 1))) ""
-    callFunc = cExprSt (cFuncall funcName $ L.map (\info -> cVar $ bufName info) bufInfo) ""
+    callFunc = funcallToTime
     body = startRDTSC ++ [callFunc] ++ endRDTSC ++ [addToTotal, incRuns]
 
-timedLoop funcName bufInfo =
+timedLoop funcallToTime =
   startRDTSC ++
-  (timedLoopBody funcName bufInfo) ++
+  (timedLoopBody funcallToTime) ++
   endRDTSC
 
-timedLoopBody funcName bufInfo =
-  [cFor (cAssign (cVar "lvar") (cIntLit 1)) (cLEQ (cVar "lvar") (cVar "num_runs")) (cAssign (cVar "lvar") (cAdd (cVar "lvar") (cIntLit 1))) (cBlock [] $ timeLoopBody funcName bufInfo) ""]
-
-timeLoopBody funcName bufInfo =
-  [cExprSt (cFuncall funcName $ L.map (\info -> cVar $ bufName info) bufInfo) ""]
+timedLoopBody funcallToTime =
+  [cFor (cAssign (cVar "lvar") (cIntLit 1)) (cLEQ (cVar "lvar") (cVar "num_runs")) (cAssign (cVar "lvar") (cAdd (cVar "lvar") (cIntLit 1))) (cBlock [] [funcallToTime]) ""]
 
 avgCyclesPerRunComputation = [cExprSt (cAssign (cVar "avg_cycles_per_run") avgVal) ""]
   where
