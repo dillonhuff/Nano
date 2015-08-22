a441a7bd20739879a157ab20c4c1af33473d047a
Dillon Huff <dillonhuff@gmail.com>
2015-08-21 23:24:03 -0500

    An example using the new AVX macros along with a leftover compiles and runs correctly. More examples will be needed for testing to be sure things are working though.

--- evalDir/fuzzTest.c
+++ evalDir/fuzzTest.c
@@ -1,104 +1,87 @@
 #include <immintrin.h>
 #include "utils.h"
 #include "avx_macros.h"
-void op(double* A, double* C, double* alpha){
-	double* tr9c9;
-	tr9c9 = malloc((sizeof(double) * 81));
-	simple_smul(9, 9, (alpha + 0), (A + 0), 1, 9, (A + 0), 1, 9);
-	simple_trans(9, 9, (tr9c9 + 0), 1, 9, (A + 0), 1, 9);
-	simple_mmul(9, 9, 9, (tr9c9 + 0), 1, 9, (tr9c9 + 0), 1, 9, (C + 0), 9, 1);
-	free(tr9c9);
+void op(double* A, double* B, double* C){
+	simple_add(13, 8, (B + 0), 8, 1, (C + 0), 8, 1, (A + 0), 8, 1);
 }
 
-void transformedOp(double* A, double* C, double* alpha){
-	int i7;
-	int i6;
-	int i9;
-	int i8;
-	int i3;
-	int i2;
-	int i1;
-	double r_0;
-	double r_1;
-	double r_2;
-	double r_3;
-	double r_4;
-	double r_5;
-	double r_6;
-	double* tr9c9;
-	tr9c9 = malloc((sizeof(double) * 81));
-	for (i7 = 0; (i7 <= 8); i7 = (i7 + 1))
+void transformedOp(double* A, double* B, double* C){
+	int i;
+	int iz;
+	__m256d r_0;
+	__m256d r_1;
+	__m256d r_2;
+	__m256d r_3;
+	__m256d r_4;
+	__m256d r_5;
+	__m256d r_0ByRow0;
+	__m256d r_0ByRow1;
+	__m256d r_0ByRow2;
+	__m256d r_0ByRow3;
+	__m256d r_1ByRow0;
+	__m256d r_1ByRow1;
+	__m256d r_1ByRow2;
+	__m256d r_1ByRow3;
+	__m256d r_2ByRow0;
+	__m256d r_2ByRow1;
+	__m256d r_2ByRow2;
+	__m256d r_2ByRow3;
+	for (i = 0; (i <= 11); i = (i + 4))
 	{
-		for (i6 = 0; (i6 <= 8); i6 = (i6 + 1))
+		for (iz = 0; (iz <= 7); iz = (iz + 4))
 		{
-			r_0 = alpha[0];
-			r_1 = A[((i6 * 1) + ((i7 * 9) + 0))];
-			r_2 = (r_0 * r_1);
-			A[((i6 * 1) + ((i7 * 9) + 0))] = r_2;
+			PACK_DBL_4x4(r_0ByRow0, r_0ByRow1, r_0ByRow2, r_0ByRow3, (B + (iz + (i * 8))), 8);
+			PACK_DBL_4x4(r_1ByRow0, r_1ByRow1, r_1ByRow2, r_1ByRow3, (C + (iz + (i * 8))), 8);
+			EADD_DBL_4x4(r_2ByRow0, r_2ByRow1, r_2ByRow2, r_2ByRow3, r_0ByRow0, r_0ByRow1, r_0ByRow2, r_0ByRow3, r_1ByRow0, r_1ByRow1, r_1ByRow2, r_1ByRow3);
+			UNPACK_DBL_4x4((A + (iz + (i * 8))), 8, r_2ByRow0, r_2ByRow1, r_2ByRow2, r_2ByRow3);
 		}
 	}
-	for (i9 = 0; (i9 <= 8); i9 = (i9 + 1))
+	for (iz = 0; (iz <= 7); iz = (iz + 4))
 	{
-		for (i8 = 0; (i8 <= 8); i8 = (i8 + 1))
-		{
-			r_3 = A[((i8 * 9) + ((i9 * 1) + 0))];
-			tr9c9[((i8 * 1) + ((i9 * 9) + 0))] = r_3;
-		}
+		r_3 = _mm256_loadu_pd((B + (iz + 96)));
+		r_4 = _mm256_loadu_pd((C + (iz + 96)));
+		r_5 = _mm256_add_pd(r_3, r_4);
+		_mm256_storeu_pd((A + (iz + 96)), r_5);
 	}
-	for (i3 = 0; (i3 <= 8); i3 = (i3 + 1))
-	{
-		for (i2 = 0; (i2 <= 8); i2 = (i2 + 1))
-		{
-			for (i1 = 0; (i1 <= 8); i1 = (i1 + 1))
-			{
-				r_4 = tr9c9[((i1 * 1) + ((i3 * 9) + 0))];
-				r_5 = tr9c9[((i2 * 9) + ((i3 * 1) + 0))];
-				r_6 = C[((i1 * 9) + ((i2 * 1) + 0))];
-				r_6 = ((r_4 * r_5) + r_6);
-				C[((i1 * 9) + ((i2 * 1) + 0))] = r_6;
-			}
-		}
-	}
-	free(tr9c9);
 }
 
 void sanity_check(FILE* df){
 	double* A;
+	double* B;
 	double* C;
-	double* alpha;
 	double* A_ref;
+	double* B_ref;
 	double* C_ref;
-	double* alpha_ref;
 	double* A_test;
+	double* B_test;
 	double* C_test;
-	double* alpha_test;
 	int A_sc_result;
+	int B_sc_result;
 	int C_sc_result;
-	int alpha_sc_result;
-	A = malloc((sizeof(double) * 81));
-	C = malloc((sizeof(double) * 81));
-	alpha = malloc((sizeof(double) * 1));
-	A_ref = malloc((sizeof(double) * 81));
-	C_ref = malloc((sizeof(double) * 81));
-	alpha_ref = malloc((sizeof(double) * 1));
-	A_test = malloc((sizeof(double) * 81));
-	C_test = malloc((sizeof(double) * 81));
-	alpha_test = malloc((sizeof(double) * 1));
-	rand_doubles(81, A);
-	rand_doubles(81, C);
-	rand_doubles(1, alpha);
-	memcpy(A_ref, A, (sizeof(double) * 81));
-	memcpy(C_ref, C, (sizeof(double) * 81));
-	memcpy(alpha_ref, alpha, (sizeof(double) * 1));
-	memcpy(A_test, A, (sizeof(double) * 81));
-	memcpy(C_test, C, (sizeof(double) * 81));
-	memcpy(alpha_test, alpha, (sizeof(double) * 1));
-	op(A_ref, C_ref, alpha_ref);
-	transformedOp(A_test, C_test, alpha_test);
-	A_sc_result = test_buffer_diff(81, A_ref, A_test);
-	C_sc_result = test_buffer_diff(81, C_ref, C_test);
-	alpha_sc_result = test_buffer_diff(1, alpha_ref, alpha_test);
-	if ((A_sc_result || (C_sc_result || alpha_sc_result)))
+	A = malloc((sizeof(double) * 104));
+	B = malloc((sizeof(double) * 104));
+	C = malloc((sizeof(double) * 104));
+	A_ref = malloc((sizeof(double) * 104));
+	B_ref = malloc((sizeof(double) * 104));
+	C_ref = malloc((sizeof(double) * 104));
+	A_test = malloc((sizeof(double) * 104));
+	B_test = malloc((sizeof(double) * 104));
+	C_test = malloc((sizeof(double) * 104));
+	rand_doubles(104, A);
+	rand_doubles(104, B);
+	rand_doubles(104, C);
+	memcpy(A_ref, A, (sizeof(double) * 104));
+	memcpy(B_ref, B, (sizeof(double) * 104));
+	memcpy(C_ref, C, (sizeof(double) * 104));
+	memcpy(A_test, A, (sizeof(double) * 104));
+	memcpy(B_test, B, (sizeof(double) * 104));
+	memcpy(C_test, C, (sizeof(double) * 104));
+	op(A_ref, B_ref, C_ref);
+	transformedOp(A_test, B_test, C_test);
+	A_sc_result = test_buffer_diff(104, A_ref, A_test);
+	B_sc_result = test_buffer_diff(104, B_ref, B_test);
+	C_sc_result = test_buffer_diff(104, C_ref, C_test);
+	if ((A_sc_result || (B_sc_result || C_sc_result)))
 	{
 		fprintf(df, "false\n");
 	}
@@ -107,8 +90,8 @@ void sanity_check(FILE* df){
 		fprintf(df, "true\n");
 	}
 	free(A);
+	free(B);
 	free(C);
-	free(alpha);
 }
 
 int main(){
--- test/SystemTests/MicroBlockedAVX.hs
+++ test/SystemTests/MicroBlockedAVX.hs
@@ -6,7 +6,7 @@ import Test.HUnit
 import CBackEnd.CodeGeneration.AVX.Common
 import Core.IndexExpression
 import Core.Statement
-import Dummies hiding (a, b, c)
+import Dummies hiding (a, b, c, d, e, f)
 import OptimizationGroups.Level1
 import TestUtils
 import Transformations.Blocking
@@ -16,9 +16,10 @@ allMicroBlockedAVXTests =
   TestList microBlockedAVXCases
 
 microBlockedAVXCases =
-  [ltc "matrix add unmatched strides" stmtsToAVX maddUnmatchedOpts maddUnmatched]
+  [ltc "matrix add no remainder" stmtsToAVX maddOpts simpleMAdd,
+   ltc "matrix add with remainder" stmtsToAVX maddOpts remainderMAdd]
 
-maddUnmatchedOpts =
+maddOpts =
   (registerization 4) ++ (tempReduction 4) ++ cleanup ++ (blockingOpts 4)
 
 blockingOpts n =
@@ -26,9 +27,15 @@ blockingOpts n =
   [(blockMatrixAddN, iConst n),
    (blockMatrixAddM, iConst n)]
 
-maddUnmatched = [matrixAdd c a b]
+simpleMAdd = [matrixAdd c a b]
 
 a = constDblMat "A" 8 8 1 8
 b = constDblMat "B" 8 8 1 8
 c = constDblMat "C" 8 8 1 8
+
+remainderMAdd = [matrixAdd f d e]
+
+f = constDblMat "A" 13 8 8 1
+d = constDblMat "B" 13 8 8 1
+e = constDblMat "C" 13 8 8 1
 
